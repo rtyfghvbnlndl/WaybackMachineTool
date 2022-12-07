074302979c94a5d145925867219ec73340b5f611
zumg <59279628+rtyfghvbnlndl@users.noreply.github.com>
2022-12-07 16:39:24 +0800

    Update index.py

--- index.py
+++ index.py
@@ -3,9 +3,10 @@ import sys
 from time import sleep
 from random import randint
 from re import findall
+import os
 urls={}#20180605 ：http://web.archive.org/web/20180605021923/https://www.bilibili.com/
 keysite="bilibili.com"
-year=2018
+year=2019
 homepage='http://web.archive.org/web/%i0101000000*/%s'% (year,keysite)
 ua={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
 
@@ -29,20 +30,18 @@ h = {
 def fWrite(content,name):
     with open("%s.txt" % name,"w+",encoding="utf-8",newline="") as f:
         f.write(str(content))
+        print('写入成功')
+
 #读档
 def fRead(name):
+    if not os.path.exists("%s.txt" % name):
+        return None
     with open("%s.txt" % name,"r",encoding="utf-8",newline="") as f:
         content=f.read()
         return content
-#空？
-def isEmpty(any):
-    if any=="" or any==[] or any=={}:
-        return True
-    else:
-        return False
 
     #是否有时日月
-if isEmpty(fRead("days")):
+if not fRead("days"):
     #没有，下载日月
     r1=requests.get("http://web.archive.org/__wb/calendarcaptures/2?url=%s&date=%i&groupby=day"% (keysite,year),headers=h)
     fWrite(r1.text,"days")
@@ -50,7 +49,7 @@ if isEmpty(fRead("days")):
 else:
     days=eval(fRead("days"))
 #检查错误
-if isEmpty(days):
+if  not days:
         print("days出错")
         sys.exit(0)
 else:
@@ -59,25 +58,30 @@ else:
 for item in days['items']:
     num1=year*10000+int(item[0]) #20180605
     #检查重复,
-    if not isEmpty(fRead("urls")):
+    if fRead("urls"):
         urls=eval(fRead("urls"))
         if num1 in list(urls.keys()):
             continue
     print("进行"+str(item[0]))
-    sleep(randint(300,600)/100)
-    r2=requests.get("http://web.archive.org/__wb/calendarcaptures/2?url=%s&date=%i"%(keysite,num1),headers=h)
-    print(r2.text)
-    if not isEmpty(r2.text):
-        num2=num1*1000000+int(eval(r2.text)["items"][0][0]) #20180605021923
-        urls[num1]="http://web.archive.org/web/%i/https://www.%s/"%(num2,keysite)
+    times=1
+    while times<=3:
+
+        sleep(randint(100,300)/100)
+        r2=requests.get("http://web.archive.org/__wb/calendarcaptures/2?url=%s&date=%i"%(keysite,num1),headers=h)
+        print(r2.text)
+        if r2.text:
+            num2=num1*1000000+int(eval(r2.text)["items"][0][0]) #20180605021923
+            urls[num1]="http://web.archive.org/web/%i/https://www.%s/"%(num2,keysite)
+            break
+        else:
+            print('空')
+            times+=1
     else:
-        print('空')
+        days['items'].remove(item)
     fWrite(urls,"urls")
 
-if isEmpty(urls):
+if not urls:
     print("urls出错")
     sys.exit(0)
 else:
     print(urls)
-
-
